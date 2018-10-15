8a10d10ac6a60cc6d31c0dff19f7d484dfdf7f6f
bobby123 <bobby1980sin@gmail.com>
2018-10-15 16:25:39 -0400

    adding new files

---
+++ python_keyword.py
@@ -0,0 +1,18 @@
+
+def calulateSum(num1,num2):
+	 return num1 + num2
+	 
+	 
+print(calulateSum(10,10))	
+
+
+
+list =[10,23,45,90]
+
+for item in list:
+	if item > 50:
+		break
+	else:
+		print(item)
+ 
+	 
